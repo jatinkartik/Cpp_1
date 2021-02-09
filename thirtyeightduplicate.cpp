ed18f116a7ee66c99bc8540f7047f2071a7f428d
jatin <jatintyagi43625@gmail.com>
2021-02-09 21:11:22 +0530

    added new file and some changes to some existing file

--- thirtyeightduplicate.cpp
+++ thirtyeightduplicate.cpp
@@ -2,15 +2,15 @@
 #include<bits/stdc++.h>
 #include<string.h>
 using namespace std;
-int pyth(int x, int y, int z);
+void pyth(int x, int y, int z);
 int main(){
     
     int x,y,z;
     cin>>x>>y>>z;
- prinft(" the value of %d",pyth(x,y,z));
+ printf(" the value of %d",pyth(x,y,z));
 return 0;
 }
-int pyth(int x, int y, int z){
+void pyth(int x, int y, int z){
      int a,b,c;
     a = max(x,max(y,z));
     if(a==x){
